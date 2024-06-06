398163182bcf8ae86dfa5be2879ba5f160cef241
Aleks <auzunov@gmail.com>
2024-06-06 15:52:42 +0200

    .

---
+++ lib/src/app.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('List App'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Enter item',
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Add Item'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
